f2676b8c3cf8b0b71e6d50f365c42b33222988c9
Juan David Franco <jfrancoh5@gmail.com>
2024-05-30 18:16:48 -0500

    Configurar el servidor

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+//coneexion a la base de datos 
 const { connection } = require('./database/connection')
-
 connection()
+
+//creacion del servidor y actvación del cors
+const express = require('express')
+const cors = require('cors')
+
+
+const app = express();
+const puerto = 3001;
+
+app.use(cors())
+
+//convertir los datos a objetos json
+app.use(express.json())   
+app.use(express.urlencoded({extended:true}))
+
+//hacer una ruta de prueba
+
+app.get('/ruta-de-prueba',function(req,res){
+
+    return res.status(200).json({
+        "id": 1,
+        "nombre":"servidor de prueba",
+        "status":"funcionando correcamente"
+    })
+})
+
+//poner al servidor a escuchar servidores http
+app.listen(puerto,()=>{
+    console.log(`Servidor corriendo en el puerto ${puerto}`)
+})
