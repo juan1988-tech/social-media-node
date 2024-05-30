83758926cc9bef51cba5fd5417a7a72dc8274ed5
Juan David Franco <jfrancoh5@gmail.com>
2024-05-30 17:22:32 -0500

    script antes de conectar a la base de datos

---
+++ arrancar-proyecto.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:/data/db" 
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongosh.exe"
+start cmd.exe /k "cd C:\Users\Juan David Franco\Documents\social-media-node && npm start"
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('me sirvo con node')
