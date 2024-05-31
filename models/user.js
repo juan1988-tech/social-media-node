c9cd67f704daf1e2e090f6a8cd756881d54cf384
Juan David Franco <jfrancoh5@gmail.com>
2024-05-31 17:15:02 -0500

    Modelos de usuarios

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const {Schema,model} = require('mongoose');
+
+const userSchema = Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    surname: String,
+    nick:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        default: 'role_user'
+    },
+    image:{
+        type: String,
+        default: 'delafult.png'
+    },
+    created_at:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = model("User", userSchema, "users")
