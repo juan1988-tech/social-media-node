d7510e208ecb448464afffc76af54499036039b6
Juan David Franco <jfrancoh5@gmail.com>
2024-06-01 16:51:11 -0500

    accion y ruta de registro de usuarios parte 1

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,6 @@
+//Importar usuarios y modulos
+const User = require('../models/user')
+
 const pruebaUser = (req,res) =>{
     return res.status(200).send({
         messasge: "mensaje enviado desde el archivo user.js"
@@ -5,9 +8,43 @@ const pruebaUser = (req,res) =>{
 }
 
 const register = (req,res)=>{
-    return res.status(200).json({
-        messasge: "Acción de registro de usuarios",
-    })
+    //recoger los parametros que nos llegan por la peticion
+    let params = req.body;
+
+    //Comprobar que los datos que me lleguen bien
+    if(!params.name || !params.email || !params.nick ){
+        console.log('petición invalida')
+        return res.status(400).json({
+            status: "error",
+            messasge: "Faltan datos por enviar",
+        })
+    }
+    let userSaved = new User(params);
+
+    //Control de usuarios duplicados
+    User.find({ $or:[
+        {email: userSaved.email.toLowerCase()},
+        {nick: userSaved.nick.toLowerCase()}
+    ]}).exec().then(
+        (error)=>{
+            if(!userSaved){
+                res.status(500).json({
+                    message: "Error en la consulta de usuarios"
+                })
+            }
+            if(userSaved && userSaved.length >=1){
+                return res.status(200).send({
+                    status: "success",
+                    message: "El usuario ya existe"
+                })
+            }
+              //Crear el objeto que guarda el usuario
+            return res.status(200).json({
+                messasge: "Acción de registro de usuarios",
+                userSaved
+            })  
+        }
+    )
 }
 
 module.exports = { pruebaUser, register } 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,6 @@ const userController = require('../controllers/users');
 
 //Definir rutas 
 router.get('/prueba-usuario',userController.pruebaUser)
-router.get('/register',userController.register)
+router.post('/register',userController.register)
 
 module.exports = router;
