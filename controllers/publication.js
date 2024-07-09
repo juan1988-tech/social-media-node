7908a51afd417ed5b98f8316fdbac71ccc44ea2c
Juan David Franco <jfrancoh5@gmail.com>
2024-07-09 16:53:43 -0500

    Guardar publicacion

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,4 +1,6 @@
 const Publication = require('../models/publication')
+const jwt = require('jwt-simple')
+const { secret } = require('../services/jwt');
 
 const pruebaPublish = (req,res) =>{
     return res.status(200).send({
@@ -6,4 +8,43 @@ const pruebaPublish = (req,res) =>{
     })
 }
 
-module.exports = { pruebaPublish } 
+const save = (req,res) =>{
+    //recoger datos del usuario
+    let params = req.body;
+
+    //sacar la identidad del usuario registrado 
+    const userToken = req.headers.authorization.replace(/["']+/g,'');
+    const identifiedUser = jwt.decode(userToken,secret);
+
+    //Si no existe el envío entonces enviamos mansaje de error
+    if(!params.text){
+        return res.status(400).send({
+            status:"failed",
+            message: "no hay datos enviados por el usuario"
+        })
+    }
+    
+    //crea el objeto de la publicacion
+    let newPublication = new Publication(params)
+    newPublication.user = identifiedUser.id
+
+    //Guardar el objeto en la base de datos
+    newPublication.save()
+    .then((newPublicItem)=>{
+        if(!newPublicItem){
+            return res.status(400).send({
+                status:"failed",
+                message: "no hay datos enviados por el usuario"
+            })
+        }
+
+        return res.status(200).send({
+            status: "success",
+            message:"publicacion realizada",
+            newPublication,    
+            identifiedUser
+        })
+    })
+}
+
+module.exports = { pruebaPublish, save } 
--- routes/publication.js
+++ routes/publication.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router =  express.Router()
 const publishController = require('../controllers/publication');
+const auth = require('../middlewares/auth');
 
 //Definir rutas 
 router.get('/prueba-publicacion',publishController.pruebaPublish)
+router.post('/save-publication',auth.auth,publishController.save)
 
 module.exports = router;
